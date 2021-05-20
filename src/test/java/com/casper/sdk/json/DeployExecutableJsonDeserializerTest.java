a9695e3debd3699149899e5b2a67c3772c286833
imills <imills@eclinicalhealth.com>
2021-05-20 17:34:34 +0100

    Added test for all transfer arguments in test data to DeployExecutableJsonDeserializerTest.java

--- src/test/java/com/casper/sdk/json/DeployExecutableJsonDeserializerTest.java
+++ src/test/java/com/casper/sdk/json/DeployExecutableJsonDeserializerTest.java
@@ -1,17 +1,18 @@
 package com.casper.sdk.json;
 
-import com.casper.sdk.domain.DeployExecutable;
-import com.casper.sdk.domain.DeployNamedArg;
+import com.casper.sdk.domain.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 /**
  * Tests a DeployExecutable can be parsed from a JSON string
@@ -19,7 +20,7 @@ import static org.hamcrest.core.IsNull.notNullValue;
 class DeployExecutableJsonDeserializerTest {
 
     // Transfer is used to indicate the type
-    private static final String SESSION_JSON = /* "session": */ """
+    private static final String TRANSFER_JSON = /* "session": */ """
             {
               "Transfer": {
                 "args": [
@@ -71,12 +72,54 @@ class DeployExecutableJsonDeserializerTest {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(module);
 
-        deployExecutable = mapper.reader().readValue(SESSION_JSON, DeployExecutable.class);
+        deployExecutable = mapper.reader().readValue(TRANSFER_JSON, DeployExecutable.class);
     }
 
+    @Test
+    void testParseTransferDeployExecutableFromJson() {
+        assertThat(deployExecutable, is(instanceOf(Transfer.class)));
+    }
 
     @Test
-    void testParseDeployExecutableFromJson() {
-        assertThat(deployExecutable, is(notNullValue()));
+    void testTransferArgs() {
+        List<DeployNamedArg> args = deployExecutable.getArgs();
+        assertThat(args.size(), is(4));
+    }
+
+    @Test
+    void testAmountArg() {
+        DeployNamedArg amount = deployExecutable.getArgs().get(0);
+        assertThat(amount.getName(), is("amount"));
+        assertThat(amount.getValue().getCLTypeInfo().getType(), is(CLType.U512));
+        assertThat(amount.getValue().getBytes(), is(CLValue.fromString("05005550b405")));
+        assertThat(amount.getValue().getParsed(), is("24500000000"));
+    }
+
+    @Test
+    void testTargetArg() {
+        DeployNamedArg target = deployExecutable.getArgs().get(1);
+        assertThat(target.getName(), is("target"));
+        assertThat(target.getValue().getCLTypeInfo().getType(), is(CLType.BYTE_ARRAY));
+        assertThat(((CLByteArrayInfo) target.getValue().getCLTypeInfo()).getSize(), is(32));
+        assertThat(target.getValue().getBytes(), is(CLValue.fromString("0101010101010101010101010101010101010101010101010101010101010101")));
+        assertThat(target.getValue().getParsed(), is("0101010101010101010101010101010101010101010101010101010101010101"));
+    }
+
+    @Test
+    void testIdArg() {
+        DeployNamedArg id = deployExecutable.getArgs().get(2);
+        assertThat(id.getName(), is("id"));
+        assertThat(id.getValue().getCLTypeInfo().getType(), is(CLType.U64));
+        assertThat(id.getValue().getBytes(), is(CLValue.fromString("01e703000000000000")));
+        assertThat(id.getValue().getParsed(), is(BigInteger.valueOf(999)));
+    }
+
+    @Test
+    void testAdditionalInfoArg() {
+        DeployNamedArg additionalInfo = deployExecutable.getArgs().get(3);
+        assertThat(additionalInfo.getName(), is("additional_info"));
+        assertThat(additionalInfo.getValue().getCLTypeInfo().getType(), is(CLType.STRING));
+        assertThat(additionalInfo.getValue().getBytes(), is(CLValue.fromString("1000000074686973206973207472616e73666572")));
+        assertThat(additionalInfo.getValue().getParsed(), is("this is transfer"));
     }
 }
