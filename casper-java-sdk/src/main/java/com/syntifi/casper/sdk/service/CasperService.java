3030a51c75ba8bf7365a5aaa29d2941693baf65b
AB <andre@syntifi.com>
2021-08-30 14:23:12 +0200

    adding methods to handle the chain_get_block_transfer RPC call

--- casper-java-sdk/src/main/java/com/syntifi/casper/sdk/service/CasperService.java
+++ casper-java-sdk/src/main/java/com/syntifi/casper/sdk/service/CasperService.java
@@ -14,6 +14,7 @@ import com.syntifi.casper.sdk.filter.block.CasperBlockByHeightFilter;
 import com.syntifi.casper.sdk.model.block.CasperBlockData;
 import com.syntifi.casper.sdk.model.peer.CasperPeerData;
 import com.syntifi.casper.sdk.model.storedvalue.CasperStoredValueData;
+import com.syntifi.casper.sdk.model.transfer.CasperTransferData;
 
 /**
  * Interface to be used as Dynamic Proxy for RPC method operation
@@ -55,7 +56,25 @@ public interface CasperService {
      * @return Object holding the api version and block
      */
     @JsonRpcMethod("chain_get_block")
-    public CasperBlockData getBlock(@JsonRpcParam("block_identifier") CasperBlockByHeightFilter heightFilter);
+    public CasperBlockData getBlock(@JsonRpcParam("block_identifier") CasperBlockByHeightFilter height);
+
+    /**
+     * Retrieve block transfers by its height
+     * 
+     * @param heightFilter Block's height
+     * @return Object holding the api version and block
+     */
+    @JsonRpcMethod("chain_get_block_transfers")
+    public CasperTransferData getBlockTransfers(@JsonRpcParam("block_identifier") CasperBlockByHeightFilter height);
+
+    /**
+     * Retrieve block transfers by its hash
+     * 
+     * @param hash Block's hash
+     * @return Object holding the api version and block
+     */
+    @JsonRpcMethod("chain_get_block_transfers")
+    public CasperTransferData getBlockTransfers(@JsonRpcParam("block_identifier") CasperBlockByHashFilter hash);
 
     /**
      * Retrieve block info by its height
