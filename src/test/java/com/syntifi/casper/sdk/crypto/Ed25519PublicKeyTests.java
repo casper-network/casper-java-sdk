1577aeebc0f95d933ba25cde273bcbacd3e73673
Alexandre Carvalho <adcarvalho@gmail.com>
2021-12-02 11:48:24 -0300

    adjustments and unicode testing

--- src/test/java/com/syntifi/casper/sdk/crypto/Ed25519PublicKeyTests.java
+++ src/test/java/com/syntifi/casper/sdk/crypto/Ed25519PublicKeyTests.java
@@ -51,11 +51,37 @@ public class Ed25519PublicKeyTests extends AbstractCryptoTests {
 
     @Test
     void verify_should_be_ok() throws URISyntaxException, IOException, GeneralSecurityException {
+        String message = "Test message";
         String hexSignature = "4555103678684364a98478112ce0c298ed841d806d2b67b09e8f0215cc738f3c5a1fca5beaf0474ff636613821bcb97e88b3b4d700e65c6cf7574489e09f170c";
 
         Ed25519PublicKey pubKey = loadPublicKey("crypto/Ed25519/public_key.pem");
 
-        assertTrue(pubKey.verify("Test message", hexSignature));
+        String hexKey = Hex.toHexString(pubKey.getKey());
+
+        LOGGER.debug("Verifying Ed25519 signature of {} with key {}", message, hexKey);
+
+        Boolean verified = pubKey.verify(message, hexSignature);
+
+        LOGGER.debug("Signature verified: {}", verified);
+
+        assertTrue(verified);
+    }
+
+    @Test
+    void verify_unicode_string() throws GeneralSecurityException {
+        String message = "أبو يوسف يعقوب بن إسحاق الصبّاح الكندي‎";
+        String hexKey = "cd62f1c5cca51fa3c25f4c76a46dd5f6b0988c95da6ea835ec4441d68dcea393";
+        String hexSignature = "c507aa390d6779d949d38258d6e1c8509664c16e645bdda9e70365ae06b23944b041aee323ea3783c923d90c73efa9c59d7b1ed87a5a1332a245874bd944fb07";
+
+        LOGGER.debug("Verifying Ed25519 signature of {} with key {}", message, hexKey);
+
+        Ed25519PublicKey pubKey = new Ed25519PublicKey(Hex.decode(hexKey));
+
+        Boolean verified = pubKey.verify(message, hexSignature);
+
+        LOGGER.debug("Signature verified: {}", verified);
+
+        assertTrue(verified);
     }
 
     private Ed25519PublicKey loadPublicKey(String publicKeyPath) throws URISyntaxException, IOException {
