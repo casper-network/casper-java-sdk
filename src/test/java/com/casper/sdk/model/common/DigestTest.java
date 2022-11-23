c3ea1adf4dd9562636c89a5228b19f757351d056
meywood <105049338+meywood@users.noreply.github.com>
2022-11-23 09:14:34 +0000

    Fix typo 2nd attempt

--- src/test/java/com/casper/sdk/model/common/DigestTest.java
+++ src/test/java/com/casper/sdk/model/common/DigestTest.java
@@ -11,7 +11,7 @@ public class DigestTest {
     }
 
     @Test
-    void testDigestIsinValid(){
+    void testDigestIsInvalid(){
         assertFalse(new Digest("9483863a391510b8d3447dd5cfc446b42d65e598672d569abc4cdded85b81e6").isValid());
         assertFalse(new Digest("uref-99483863a391510b8d3447dd5cfc446b42d65e598672d569abc4cdded85b81e6").isValid());
     }
