d8d648a7ad9071c4559ca889cf93272954540c87
meywood <105049338+meywood@users.noreply.github.com>
2023-09-22 12:14:29 +0100

    issues/216 - Change json resource path

--- src/test/java/com/casper/sdk/e2e/utils/AssetUtils.java
+++ src/test/java/com/casper/sdk/e2e/utils/AssetUtils.java
@@ -1,6 +1,9 @@
 package com.casper.sdk.e2e.utils;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 /**
@@ -18,5 +21,11 @@ public class AssetUtils {
     public static URL getFaucetAsset(final int networkId, final String keyFilename) {
         String path = String.format("/net-%d/faucet/%s", networkId, keyFilename);
         return Objects.requireNonNull(AssetUtils.class.getResource(path), "missing resource " + path);
+    }
+
+    public static URL getStandardTestResourceURL(String jsonFilename) throws MalformedURLException {
+        final Path currentRelativePath = Paths.get("");
+        final String jsonPath = "file://" + currentRelativePath.toAbsolutePath() + "/sdk-std-test-resources" + jsonFilename;
+        return new URL(jsonPath);
     }
 }
