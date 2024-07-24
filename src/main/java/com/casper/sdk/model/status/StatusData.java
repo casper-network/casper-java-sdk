504ab49a8486e74663025bcdbb9d1d42142206b2
meywood <105049338+meywood@users.noreply.github.com>
2024-07-24 10:57:19 +0100

    issues/319 - Fixes for info_get_status response data StatusData

--- src/main/java/com/casper/sdk/model/status/StatusData.java
+++ src/main/java/com/casper/sdk/model/status/StatusData.java
@@ -26,52 +26,34 @@ import java.util.List;
 @NoArgsConstructor
 public class StatusData extends RpcResult {
 
-    /**
-     * The compiled node version
-     */
+    /** The compiled node version */
     @JsonProperty("build_version")
     private String buildVersion;
 
-    /**
-     * The chainspec name
-     */
+    /** The chainspec name */
     @JsonProperty("chainspec_name")
     private String chainSpecName;
 
-    /**
-     * @see MinimalBlockInfo
-     */
+    /** @see MinimalBlockInfo */
     @JsonProperty("last_added_block_info")
     private MinimalBlockInfo lastAddedBlockInfo;
 
-    /**
-     * @see NextUpgrade
-     */
+    /** @see NextUpgrade */
     @JsonProperty("next_upgrade")
     private NextUpgrade nextUpgrade;
 
-    /**
-     * @see PublicKey
-     */
+    /** @see PublicKey */
     @JsonProperty("our_public_signing_key")
     private PublicKey publicKey;
 
-    /**
-     * List of
-     *
-     * @see PeerEntry
-     */
+    /** List of * * @see PeerEntry */
     private List<PeerEntry> peers;
 
-    /**
-     * The next round length if this node is a validator
-     */
+    /** The next round length if this node is a validator */
     @JsonProperty("round_length")
     private String roundLength;
 
-    /**
-     * Time passed since the node has started
-     */
+    /** Time passed since the node has started */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String uptime;
 
@@ -81,38 +63,29 @@ public class StatusData extends RpcResult {
     @JsonProperty("starting_state_root_hash")
     private String startStateRootHash;
 
-    /**
-     * Node run mode
-     */
+    /** Node run mode */
     @JsonProperty("node_state")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private NodeState nodeState;
 
-    /**
-     * The state of the reactor
-     */
+    /** The state of the reactor */
     @JsonProperty("reactor_state")
     private ReactorState reactorState;
 
-    /**
-     * Timestamp of the last recorded progress in the reactor
-     */
+    /** Timestamp of the last recorded progress in the reactor */
     @JsonProperty("last_progress")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private Date lastProgress;
 
-    /**
-     * The available block range in storage
-     */
+    /** The available block range in storage */
     @JsonProperty("available_block_range")
     private AvailableBlockRange availableBlockRange;
 
-    /**
-     * The status of the block synchronizer builders
-     */
+    /** The status of the block synchronizer builders */
     @JsonProperty("block_sync")
     private BlockSynchronizerStatus blockSynchronizerStatus;
 
+    /** The hash of the latest switch block. */
     @JsonProperty("latest_switch_block_hash")
     private Digest lastSwitchBlockHash;
 }
