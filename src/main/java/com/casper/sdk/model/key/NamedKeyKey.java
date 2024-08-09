c0e91de8c7f6b7c554749c0325cd6bc924bab6a5
meywood <105049338+meywood@users.noreply.github.com>
2024-08-09 12:40:19 +0100

    issues/325 - String serialization for NamedKeyKey

--- src/main/java/com/casper/sdk/model/key/NamedKeyKey.java
+++ src/main/java/com/casper/sdk/model/key/NamedKeyKey.java
@@ -1,6 +1,9 @@
 package com.casper.sdk.model.key;
 
+import com.casper.sdk.exception.NoSuchKeyTagException;
+import com.syntifi.crypto.key.encdec.Hex;
 import dev.oak3.sbs4j.DeserializerBuffer;
+import dev.oak3.sbs4j.SerializerBuffer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +26,33 @@ public class NamedKeyKey extends Key {
     private AddressableEntityKey baseAddr;
     /** The bytes of the name. */
     private byte[] stringBytes;
+
+    @Override
+    protected void fromStringCustom(final String strKey) {
+        final String[] split = strKey.split("-");
+        try {
+            final String baseAddrStr = split[2] + "-" + split[3] + "-" + split[4];
+            baseAddr = (AddressableEntityKey) Key.fromKeyString(baseAddrStr);
+            stringBytes = Hex.decode(split[5]);
+
+            final SerializerBuffer ser = new SerializerBuffer();
+            ser.writeU8(baseAddr.getEntityAddressTag().getByteTag());
+            ser.writeByteArray(baseAddr.getKey());
+            ser.writeByteArray(stringBytes);
+            setKey(ser.toByteArray());
+        } catch (NoSuchKeyTagException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+
+
+    @Override
+    public String toString() {
+        return this.getTag().getKeyName() +
+                baseAddr + "-" +
+                Hex.encode(stringBytes);
+    }
 
     @Override
     protected void deserializeCustom(final DeserializerBuffer deser) throws Exception {
