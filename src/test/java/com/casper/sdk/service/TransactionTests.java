956765f1b6fe04815837c04a5b323b34fa8fd255
meywood <105049338+meywood@users.noreply.github.com>
2024-09-03 10:46:19 +0100

    issues/330 - Disable all tests that require a node

--- src/test/java/com/casper/sdk/service/TransactionTests.java
+++ src/test/java/com/casper/sdk/service/TransactionTests.java
@@ -22,6 +22,7 @@ import com.syntifi.crypto.key.AbstractPublicKey;
 import com.syntifi.crypto.key.Ed25519PrivateKey;
 import dev.oak3.sbs4j.exception.ValueSerializationException;
 import org.apache.cxf.helpers.IOUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -44,7 +45,7 @@ import static org.hamcrest.core.IsNull.notNullValue;
  *
  * @author ian@meywood.com
  */
-//@Disabled
+@Disabled
 public class TransactionTests {
 
     @Test
