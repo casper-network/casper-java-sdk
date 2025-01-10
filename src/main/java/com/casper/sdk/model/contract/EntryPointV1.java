78619bfd99f34cc536591d0959bc76ed7832127a
meywood <105049338+meywood@users.noreply.github.com>
2025-01-10 11:55:55 +0000

    issues/373 - Updated contract to user condor style entrt point classes.

---
+++ src/main/java/com/casper/sdk/model/contract/EntryPointV1.java
@@ -0,0 +1,33 @@
+package com.casper.sdk.model.contract;
+
+import com.casper.sdk.jackson.deserializer.EntryPointV1Deserializer;
+import com.casper.sdk.model.clvalue.cltype.AbstractCLType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * Type signature of a method. Order of arguments matter since can be referenced by index as well as name.
+ *
+ * @author ian@meywood.com
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonTypeName("entry_point")
+@JsonDeserialize(using = EntryPointV1Deserializer.class)
+public class EntryPointV1 {
+
+    private String name;
+    private String access;
+    private AbstractCLType ret;
+    @JsonProperty("entry_point_type")
+    private EntryPointType entryPointType;
+    @JsonProperty("args")
+    private List<EntryPointArg> args;
+}
