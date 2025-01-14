675b9d34056bf3bf7786d280386f5264a957bfac
meywood <105049338+meywood@users.noreply.github.com>
2025-01-14 14:10:47 +0000

    issues/379 - Corrected JSON serialization for delegator kinds and fixed test data in error.

--- src/test/java/com/casper/sdk/model/deploy/DeployDataTests.java
+++ src/test/java/com/casper/sdk/model/deploy/DeployDataTests.java
@@ -24,6 +24,28 @@ public class DeployDataTests extends AbstractJsonTests {
     private static final Logger LOGGER = LoggerFactory.getLogger(DeployDataTests.class);
 
     @Test
+    void validateDeployDataMapping_1() throws IOException, JSONException {
+        // curl -X POST -H 'Content-Type: application/json' -d
+        // '{"jsonrpc":"2.0","id":"1","method":"info_get_deploy",
+        // "params":{"deploy_hash":"614030ac705ed2067fed57d30545b3a4974ffc40a1c32f72e3b7b7442d6c83a3"}
+        // }' http://nodeIP:7777/rpc
+        String inputJson = getPrettyJson(loadJsonFromFile("deploy-samples/deploy-v1.json"));
+
+        LOGGER.debug("Original JSON: {}", inputJson);
+
+        DeployData dd = OBJECT_MAPPER.readValue(inputJson, DeployData.class);
+
+        assertNotNull(dd.getDeploy());
+        assertNotNull(dd.getExecutionInfo().getExecutionResult());
+
+        String expectedJson = getPrettyJson(dd);
+
+        LOGGER.debug("Serialized JSON: {}", expectedJson);
+
+        JSONAssert.assertEquals(inputJson, expectedJson, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
     void validateDeployDataMapping_2() throws IOException, JSONException {
         String inputJson = getPrettyJson(loadJsonFromFile("deploy-samples/deploy-v2.json"));
 
