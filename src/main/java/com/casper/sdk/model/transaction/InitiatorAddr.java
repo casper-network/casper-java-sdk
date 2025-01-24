554a56a39a9b8e3a5541f3bdb31dba2de4f02bc9
meywood <105049338+meywood@users.noreply.github.com>
2025-01-24 12:26:06 +0000

    issues/388 - Update InitiatorAddr byte serialization to match condor changes

--- src/main/java/com/casper/sdk/model/transaction/InitiatorAddr.java
+++ src/main/java/com/casper/sdk/model/transaction/InitiatorAddr.java
@@ -1,15 +1,10 @@
 package com.casper.sdk.model.transaction;
 
-import com.casper.sdk.exception.NoSuchTypeException;
 import com.casper.sdk.model.clvalue.serde.CasperSerializableObject;
-import com.casper.sdk.model.clvalue.serde.Target;
 import com.casper.sdk.model.key.Tag;
-import com.casper.sdk.model.transaction.field.CalltableSerializationEnvelopeBuilder;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonValue;
-import dev.oak3.sbs4j.SerializerBuffer;
-import dev.oak3.sbs4j.exception.ValueSerializationException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,17 +27,9 @@ public abstract class InitiatorAddr<T> implements CasperSerializableObject, Tag 
 
     protected static final int PUBLIC_KEY_TAG = 0;
     protected static final int ACCOUNT_HASH_TAG = 1;
-    private static final int TAG_FIELD_INDEX = 0;
-    private static final int ADDR_KEY_FIELD_INDEX = 1;
+    protected static final int TAG_FIELD_INDEX = 0;
+    protected static final int ADDR_KEY_FIELD_INDEX = 1;
 
     @JsonValue
     private T address;
-
-    @Override
-    public void serialize(final SerializerBuffer ser, final Target target) throws ValueSerializationException, NoSuchTypeException {
-        new CalltableSerializationEnvelopeBuilder()
-                .addField(TAG_FIELD_INDEX,  /* U8 */  getByteTag())
-                .addField(ADDR_KEY_FIELD_INDEX, ((CasperSerializableObject) getAddress()))
-                .serialize(ser, target);
-    }
 }
